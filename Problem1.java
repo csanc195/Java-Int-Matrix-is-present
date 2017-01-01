08e081d02b58eb8cd8badc8ff17cc757df5deea5
Carlos <csanc195@fiu.edu>
2017-01-01 00:21:53 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,30 +1,4 @@
-/********************************************************************* 
-	Purpose/Description: 
-		The following program includes a simple class with a main algorithm that proposes 
- 	a solution of linear complexity (O(n)) to find a certain number/element of the primitive 
- 	type int in an ordered matrix of integer numbers.This matrix is a matrix on which each row 
- 	and column is ordered in descending order.
- 		The algorithm:
- 			The main idea behind the functionality of the included algorithm is to use the fact that
- 		 the matrix is ordered by row and column in descending order to our advantage. By choosing 
- 		 carefully a starting point for the search and choosing the right checks the program can
- 		 easily find the desired target (if present) or traverse the matrix and return false quickly
-	     if the target is not present.
-	     In this case, I chose to start at the top right corner, starting at this point allows for the 
-	     assumption that at any given position in the matrix, matrix[xi][yj], there will always be a larger
-	     element to the left of: matrix[xi][yj] and a smaller element directly under: matrix[xi][yj]; unless
-	     the current tuple is located at the edge, in which case nature of this matrix will ensure that while 
-	     making the right comparisons between the target and the current tuple, the program will traverse
-	     the matrix without the risk of trying to access out of bounds indexes. The program will return true
-	     if the target is ever found, or false if the matrix is null/empty or if the element is either greater
-	     than the greatest element present(I) or smaller than the smallest element present(II). 
-	     Statements (I) and (II) are inductive assumptions that are a result of the ordering of the  given 
-	     matrix, which as a side effect of the ordering will have the largest element located at position[0][0] and
-	     the smallest element at position [m][n]. Where m and n are the values of the rows and columns of the matrix.
-	Author’s Panther ID: <5446178>
-	Certification:
-	I hereby certify that this work is my own and none of it is the work of any other person.
- ********************************************************************/
+
 /**
  * This is simple public class, class could be easily redefined to be final and
  * to take matrices of any types (ordered by row and column) and find a certain element.
